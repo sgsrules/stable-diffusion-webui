f87ad2038f8ee9ef0734f485cfb1aaf0a942c474
sgsrules <sgs.rules@gmail.com>
2022-11-17 13:12:41 -0600

    Update renameCollection.py

--- renameCollection.py
+++ renameCollection.py
@@ -7,7 +7,7 @@ path = r'F:\CodeProjects\stable-diffusion-webui\outputs\Flowed\Beat Droids\\'
 dir_list = glob.glob(path+"*.*")
 #dir_list = os.listdir(path)
 filelist = []
-filenames = "Skything,Toptrap,Fangclaw,Jetwar,Growlslide,Weasel,Harness,Wreckage,Flinch,Prodigy,Heavyguard,Retroray,Hydraubite,Growllight,Longguard,Barrage,Sunblast,Snarl,Inferno,Core,Magmastrong,Flamerazor,Chromestream,Dreadhorn,Hailburner,Thunder,Comet,Gadget,Pursuit,Fury,Clifftop,Shockwar,Junkline,Stonedrive,Grimflight,Sky-High,Element,Crossflare,Scratch,Mercy,Deepheap,Meanclaw,Haillock,Twinclutch,Lockkick,Shamble,Torrent,Nightlight,Dread,Mercy,Stormheap,Doublestalker,Steelscraps,Flypunch,Longworks,Fungus,Rush,Prodigy,Residue,Aries,Neutroflow,Hookburner,Blowblaze,Shadowworks,Roadback,Crossflare,Quake,Wallop,Twinkle,Inferno"
+filenames = "Temjir,Ebium,Surtar,Useus,Vostus,Vurasil,Rhumos,Zania,Moorr,Qhiborh,Rumphion,Briasis,Qhisoi,Qrohrena,Xorla,Myhdona,Iotz,Qutraura,Duanke,Thotana,Vetar,Ikies,Koteus,Qhysnir,Bivmis,Iean,Qheohr,Qhidall,Geas,Zasyn,Vetelia,Xearae,Yneas,Dotrena,Nera,Kruasis,Eyja,Ekta,Enera,Xegtix,Wides,Chimes,Fodagi,Qeher,Osemis,Cedbris,Xadur,Ortar,Ixses,Ynia"
 filenames = filenames.split(',')
 i = 0
 for file in dir_list:
